baf7c443f514e37ff7b8b21bb2d546b721d6c930
Thobani Luthuli <Thobanil@privateproperty.co.za>
2023-06-19 17:58:01 +0200

    Added entity framwork and created database

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext: DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Hotel>().HasData(new Hotel { Id = 1, Name="Sandals Resort and Spar", Address = "Negril", Rating = 4.5, CountryId = 1},
+                new Hotel { Id = 2, Name = "Comfort Suites", Address = "Geoge Town", Rating = 4.3, CountryId = 3 },
+                new Hotel { Id = 3, Name = "Grand Palldium", Address = "Nassua", Rating = 4, CountryId = 2 });
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM",
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS",
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Cayman Island",
+                    ShortName = "CI",
+                });
+        }
+    }
+}
